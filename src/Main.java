3fadc16c37c1677607a49b22bdb6695a244d9d52
MuhammedHayta <aymermu@hotmail.com>
2023-04-07 21:20:11 +0300

    started with the basics, further work and testing is required

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,104 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.util.*;
+
+import static java.lang.Math.pow;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+
+    public static void main(String args[]) throws Exception{
+        File file = new File("input.txt");
+        FileWriter output = new FileWriter("output.txt");
+
+        if (!file.exists())
+            throw new Exception("input file does not exsist: " + file.getName());
+
+        Scanner fileInput = new Scanner(file);
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Byte ordering: ");
+        String endian = input.next();
+
+        System.out.println("Data type: ");
+        String dType = input.next();
+
+        System.out.println("Data type size: ");
+        String dSize = input.next();
+
+        int size = dSize.charAt(0);
+
+        String line = "";
+
+        // Note: unnecessary checks, change to for loops in if
+        while(fileInput.hasNextLine()){
+            line = CheckEndian( endian, fileInput.nextLine().trim() );
+
+            for(int i = 0;line.length() - i != 0; i += size*2){
+                //line.substring(i,i+size*2);
+
+                if(dType.equalsIgnoreCase("Floating point"))
+                    line.substring(i,i+size*2);
+                else if (dType.equalsIgnoreCase("Signed integer"))
+                    output.write( Convert2Decimal4Signed( line.substring(i,i+size*2) ) );
+                else if(dType.equalsIgnoreCase("Unsigned integer"))
+                    line.substring(i,i+size*2);
+                else
+                    throw new Exception("invalid data type: " + line);
+
+
+
+            }
+            // Maybe switch case instead of if else
+            /*
+            switch (dType) {
+                case "Floating point":
+                    // call function in loop
+                    break;
+                }
+
+                 */
+        }
+
+
+
+
     }
+    // Note for this part's appointed person: you can change the function names as you like
+    public static String CheckEndian(String endianType, String hexNumber){
+
+        return hexNumber;
+    }
+
+    public static int Convert2Decimal4Signed(String hexNumber){
+
+        String binary = Hex2Binary(hexNumber);
+        int value = 0;
+        if(binary.charAt(0) == '1')
+            value = (int)( -pow(2, binary.length()-1) );
+
+        for(int i = 1; i<binary.length(); i++){
+            if(binary.charAt(i) == '1')
+                value += (int)pow(2,binary.length()-i-1);
+        }
+        return value;
+
+    }
+    // Note: not sure if this will work
+    public static String Hex2Binary(String hexNumber){
+
+        String binary = "";
+
+        for(int i = 0; i < hexNumber.length() ;i++) {
+
+            for (int j = 0; j < 4; j++) {
+                binary = Integer.toString( (int)( (hexNumber.charAt(i)-'0')% pow(2,j) ) ).concat( binary );
+
+            }
+        }
+        return binary;
+    }
+
 }
